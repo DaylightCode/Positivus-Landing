4c814a6f5b38a661c9f5d260014540712c39ca67
DaylightCode <dayiight@proton.me>
2025-12-11 12:57:17 +0300

    Update Testimonials.tsx

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -57,7 +57,7 @@ export default function Testimonials() {
   const testimonials = [
     {
       name: 'John Smith',
-      position: 'Marketing Director at ZYX Corp',
+      position: 'Marketing Director at XYZ Corp',
       description: '"We have been working with Positivus for the past year and have seen a significant increase in website traffic and leads as a result of their efforts. The team is professional, responsive, and truly cares about the success of our business. We highly recommend Positivus to any company looking to grow their online presence."',
     },
     {
